b951712dfb58029568ce7fa0e4a5f9a968689688
rakibul12212 <rakibul15-12212@diu.edu.bd>
2024-03-06 01:33:34 +0600

    protected route set up

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,30 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from './features/auth/authSlice'
 import { baseApi } from "./api/baseApi";
+import {persistReducer,persistStore, FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const persistConfig ={
+    key:'auth',
+    storage
+}
+const persistedAuthReducer = persistReducer(persistConfig,authReducer);
 
 export const store = configureStore({
     reducer:{
        [baseApi.reducerPath]:baseApi.reducer,
-        auth:authReducer,
+        auth: persistedAuthReducer,
     },
-    middleware: (getDefaultMiddlewares )=> getDefaultMiddlewares().concat(baseApi.middleware),
+    middleware: (getDefaultMiddlewares )=> getDefaultMiddlewares({
+        serializableCheck:{
+          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],  
+        }
+    }).concat(baseApi.middleware),
 })
 
 
@@ -15,3 +32,5 @@ export const store = configureStore({
 export type RootState = ReturnType<typeof store.getState>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch
+
+export const persistor =persistStore(store) ;
